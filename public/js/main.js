8ee00b5cdd7170014e9a23b098b0b83cedbaca0a
ryno <ryno412@gmail.com>
2014-10-05 21:22:57 -0700

    adding simple view

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,28 @@
+var SimpleApp = {};
+
+(function () {
+
+    function IndexView () {};
+
+    IndexView.prototype = {
+
+        init : function () {
+         this.initEvents();
+        },
+        initEvents : function () {
+           $('#do-something').click(function (e) {
+              console.log("we got one");
+           });
+        }
+
+    };
+
+    SimpleApp.IndexView = IndexView;
+
+})();
+
+
 $(document).ready(function (){
-    console.log('yo');
-})
+    var indexView = new SimpleApp.IndexView();
+    indexView.init();
+});
