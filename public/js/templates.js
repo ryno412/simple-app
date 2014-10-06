b3a53ee7cbfeeb62adf1dd21e290c7f6a512f3c1
ryno <ryno412@gmail.com>
2014-10-06 09:35:12 -0700

    hb template

---
+++ public/js/templates.js
@@ -0,0 +1,9 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['home'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+  return "<h1>hello "
+    + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
+    + "</h1>";
+},"useData":true});
+})();
